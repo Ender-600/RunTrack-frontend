a5b835c7c599e7a7ff2910ef0e378d441a91f857
Maxwell <boyul5@illinois.edu>
2024-12-10 08:54:52 -0600

    fix event page and profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import ShopPage from "./pages/shop/ShopPage";
+import EventPage from "./pages/EventPage/EventPage";
 import LoginRegister from "./features/auth/LoginRegister";
 import Navbar from "./components/Navbar";
+import Run from "./pages/run/Run";
 import PrivateRoute from "./features/auth/PrivateRoute";
 import ProfilePage from "./pages/profile/ProfilePage";
+import CreateEventPage from "./pages/EventPage/CreateEventPage";
 
 function App() {
+    const [value, setValue] = useState(0);
+
+    const handleNavChange = (newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <Router>
             <Routes>
@@ -20,8 +30,12 @@ function App() {
                     }
                 />
                 <Route path="/home" element={<Home />} />
+                <Route path="/shop" element={<ShopPage />} />
+                <Route path="/events" element={<EventPage />} />
+                <Route path="/run" element={<Run />} />
+                <Route path="/create-event" element={<CreateEventPage />} />
             </Routes>
-            <Navbar />
+            <Navbar value={value} onChange={handleNavChange} />
         </Router>
     );
 }
